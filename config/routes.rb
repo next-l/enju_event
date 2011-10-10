24bbe81a92171010a745c8303bba0402038ffd5b
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-10 22:02:47 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  resources :events do
+    resources :picture_files
+  end
+  resources :event_categories
+  resources :libraries do
+    resources :events
+  end
 end
