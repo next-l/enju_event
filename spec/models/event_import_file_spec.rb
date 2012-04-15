93e3c3f3a468b9d8902b236b0c980834b0a7e4a2
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-04-15 21:49:19 +0900

    updated locale files

--- spec/models/event_import_file_spec.rb
+++ spec/models/event_import_file_spec.rb
@@ -78,17 +78,19 @@ end
 #  parent_id                 :integer
 #  content_type              :string(255)
 #  size                      :integer
-#  file_hash                 :string(255)
 #  user_id                   :integer
 #  note                      :text
-#  imported_at               :datetime
+#  executed_at               :datetime
 #  state                     :string(255)
 #  event_import_file_name    :string(255)
 #  event_import_content_type :string(255)
 #  event_import_file_size    :integer
 #  event_import_updated_at   :datetime
-#  created_at                :datetime
-#  updated_at                :datetime
 #  edit_mode                 :string(255)
+#  event_import_fingerprint  :string(255)
+#  created_at                :datetime        not null
+#  updated_at                :datetime        not null
+#  event_fingerprint         :string(255)
+#  error_message             :text
 #
 
