a735458be19a0415cbfb392ce545316382574901
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-16 01:08:08 +0900

    updated attachment path

--- app/models/event_import_file.rb
+++ app/models/event_import_file.rb
@@ -10,7 +10,8 @@ class EventImportFile < ActiveRecord::Base
       :path => "event_import_files/:id/:filename",
       :s3_permissions => :private
   else
-    has_attached_file :event_import, :path => ":rails_root/private:url"
+    has_attached_file :event_import,
+      :path => ":rails_root/private/system/:class/:attachment/:id_partition/:style/:filename"
   end
   validates_attachment_content_type :event_import, :content_type => ['text/csv', 'text/plain', 'text/tab-separated-values', 'application/octet-stream']
   validates_attachment_presence :event_import
