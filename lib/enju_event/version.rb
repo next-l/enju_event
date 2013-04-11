beac85e2043606b2de35f44e6584d59750f81f25
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 02:35:43 +0900

    updated generator

--- lib/enju_event/version.rb
+++ lib/enju_event/version.rb
@@ -1,3 +1,3 @@
 module EnjuEvent
-  VERSION = "0.1.17.pre9"
+  VERSION = "0.1.17.pre10"
 end
--- lib/generators/enju_event/setup/setup_generator.rb
+++ lib/generators/enju_event/setup/setup_generator.rb
@@ -4,5 +4,7 @@ class EnjuEvent::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_event")
     rake("enju_event_engine:install:migrations")
+    inject_into_file 'app/controllers/application_controller.rb',
+      "  enju_event\n", :after => "enju_library\n"
   end
 end
