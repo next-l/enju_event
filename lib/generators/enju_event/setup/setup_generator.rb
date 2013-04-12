5d77c28c7d286234ea703371b69e407915f9d371
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 16:39:08 +0900

    updated generator

--- lib/generators/enju_event/setup/setup_generator.rb
+++ lib/generators/enju_event/setup/setup_generator.rb
@@ -6,5 +6,9 @@ class EnjuEvent::SetupGenerator < Rails::Generators::Base
     rake("enju_event_engine:install:migrations")
     inject_into_file 'app/controllers/application_controller.rb',
       "  enju_event\n", :after => "enju_library\n"
+    inject_into_file 'app/assets/stylesheets/application.css',
+      " *= require event_calendar\n", :after => " *= require enju_leaf\n"
+    inject_into_file 'app/assets/javascripts/application.js',
+      "//= require event_calendar\n", :after => "//= require enju_leaf\n"
   end
 end
