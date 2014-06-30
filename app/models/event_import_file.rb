dcad266da8cbbf51d96df6b7be0d2528b40f57f4
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-30 15:45:23 +0900

    updated attr_accessible

--- app/models/event_import_file.rb
+++ app/models/event_import_file.rb
@@ -1,7 +1,7 @@
 class EventImportFile < ActiveRecord::Base
   include Statesman::Adapters::ActiveRecordModel
   include ImportFile
-  attr_accessible :event_import, :edit_mode, :user_encoding
+  attr_accessible :event_import, :edit_mode, :user_encoding, :mode
   default_scope {order('event_import_files.id DESC')}
   scope :not_imported, -> {in_state(:pending)}
   scope :stucked, -> {in_state(:pending).where('created_at < ?', 1.hour.ago)}
