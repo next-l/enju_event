5c18c274640b47e852d00422614362449750a03a
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-04 03:50:41 +0900

    csv -> tsv

--- app/controllers/event_import_results_controller.rb
+++ app/controllers/event_import_results_controller.rb
@@ -1,5 +1,5 @@
 class EventImportResultsController < InheritedResources::Base
-  respond_to :html, :json, :csv
+  respond_to :html, :json, :tsv
   load_and_authorize_resource
   has_scope :file_id
   actions :index, :show, :destroy
