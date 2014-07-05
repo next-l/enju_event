6dc01451509e2d86e38e504d2a50461d564c3c70
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-05 13:10:08 +0900

    start import when a file is uploaded
    Conflicts:
    	app/controllers/event_import_files_controller.rb

--- app/controllers/event_import_files_controller.rb
+++ app/controllers/event_import_files_controller.rb
@@ -44,18 +44,17 @@ class EventImportFilesController < ApplicationController
   # POST /event_import_files
   # POST /event_import_files.json
   def create
+    authorize EventImportFile
     @event_import_file = EventImportFile.new(event_import_file_params)
-    authorize @event_import_file
     @event_import_file.user = current_user
 
-    respond_to do |format|
-      if @event_import_file.save
-        format.html { redirect_to @event_import_file, :notice => t('controller.successfully_created', :model => t('activerecord.models.event_import_file')) }
-        format.json { render :json => @event_import_file, :status => :created, :location => @event_import_file }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @event_import_file.errors, :status => :unprocessable_entity }
+    if @event_import_file.save
+      if @event_import_file.mode == 'import'
+        Resque.enqueue(EventImportFileQueue, @event_import_file.id)
       end
+      redirect_to @event_import_file, notice: t('controller.successfully_created', :model => t('activerecord.models.event_import_file'))
+    else
+      render action: 'new'
     end
   end
 
