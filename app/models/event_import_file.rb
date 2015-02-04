98ac3fa0e7616be4515b11034514ef3e934d10ef
Kosuke Tanabe <nabeta@fastmail.fm>
2015-02-04 23:47:50 +0900

    fixed setting s3_credentials

--- app/models/event_import_file.rb
+++ app/models/event_import_file.rb
@@ -7,7 +7,7 @@ class EventImportFile < ActiveRecord::Base
 
   if Rails.application.config_for(:enju_leaf)["uploaded_file"]["storage"] == :s3
     has_attached_file :event_import, storage: :s3,
-      s3_credentials: "#{Rails.application.config_for(:enju_leaf)["amazon"]}",
+      s3_credentials: Rails.application.config_for(:enju_leaf)["amazon"],
       s3_permissions: :private
   else
     has_attached_file :event_import,
