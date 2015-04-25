e3aeac57b768d244b340a3c4f12fa28db64351b3
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-25 14:19:59 +0900

    fixed migration file

--- db/migrate/20150305001006_add_event_export_id_to_event_export_file.rb
+++ db/migrate/20150305001006_add_event_export_id_to_event_export_file.rb
@@ -1,9 +1,8 @@
 class AddEventExportIdToEventExportFile < ActiveRecord::Migration
   def change
     add_column :event_export_files, :event_export_id, :string
-    add_column :event_export_files, :event_export_file_name, :string
     add_column :event_export_files, :event_export_size, :integer
-    add_column :event_export_files, :event_export_content_type, :string
+    rename_column :event_export_files, :event_export_file_name, :event_export_filename
     add_index :event_export_files, :event_export_id
   end
 end
