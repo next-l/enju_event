37e8173c9c755877bca62aefb441f886d0e18631
Kosuke Tanabe <nabeta@fastmail.fm>
2015-11-22 23:21:51 +0900

    remove worker files

--- app/workers/event_export_file_queue.rb
+++
@@ -1,7 +0,0 @@
-class EventExportFileQueue
-  @queue = :event_export_file
-
-  def self.perform(event_export_id)
-    EventExportFile.find(event_export_id).export!
-  end
-end
--- app/workers/event_import_file_queue.rb
+++
@@ -1,7 +0,0 @@
-class EventImportFileQueue
-  @queue = :event_import_file
-
-  def self.perform(event_import_file_id)
-    EventImportFile.find(event_import_file_id).import_start
-  end
-end
