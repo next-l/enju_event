6a5969ee98fd01f93c5c2bd2b92bf13a387c99b0
Kosuke Tanabe <nabeta@fastmail.fm>
2016-07-04 03:01:28 +0900

    update devise helper

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include EnjuBiblio::Controller
   include EnjuLibrary::Controller
   include EnjuEvent::Controller
+  before_action :set_paper_trail_whodunnit
   after_action :verify_authorized
 
   include Pundit
