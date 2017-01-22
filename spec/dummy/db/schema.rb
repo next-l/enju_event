6cfa1091bb3963056a26ffa102b97264fcbefb0e
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-22 12:34:03 +0900

    update example files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -930,11 +930,11 @@ ActiveRecord::Schema.define(version: 20170121173222) do
 
   create_table "message_request_transitions", force: :cascade do |t|
     t.string   "to_state"
-    t.text     "metadata",           default: "{}"
+    t.jsonb    "metadata",           default: {}
     t.integer  "sort_key"
     t.integer  "message_request_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.boolean  "most_recent"
     t.index ["message_request_id"], name: "index_message_request_transitions_on_message_request_id", using: :btree
     t.index ["sort_key", "message_request_id"], name: "index_message_request_transitions_on_sort_key_and_request_id", unique: true, using: :btree
@@ -964,11 +964,11 @@ ActiveRecord::Schema.define(version: 20170121173222) do
 
   create_table "message_transitions", force: :cascade do |t|
     t.string   "to_state"
-    t.text     "metadata",    default: "{}"
+    t.jsonb    "metadata",    default: {}
     t.integer  "sort_key"
     t.integer  "message_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.boolean  "most_recent"
     t.index ["message_id"], name: "index_message_transitions_on_message_id", using: :btree
     t.index ["sort_key", "message_id"], name: "index_message_transitions_on_sort_key_and_message_id", unique: true, using: :btree
