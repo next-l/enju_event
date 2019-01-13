130efba1b207a2117461058c50209e20101f3360
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-14 02:37:55 +0900

    update CI config

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,6 +7,12 @@ require 'enju_event'
 
 module Dummy
   class Application < Rails::Application
+<<<<<<< HEAD
+=======
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+>>>>>>> febdc8c... update CI config
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
