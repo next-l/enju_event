b4751ad228a98f5ad75dc55aa84254b7bb364a67
Kosuke Tanabe <nabeta@fastmail.fm>
2019-07-13 17:27:37 +0900

    update generator

--- lib/generators/enju_event/setup/setup_generator.rb
+++ lib/generators/enju_event/setup/setup_generator.rb
@@ -6,9 +6,9 @@ class EnjuEvent::SetupGenerator < Rails::Generators::Base
     rake("enju_event_engine:install:migrations")
     inject_into_file 'app/controllers/application_controller.rb',
       "  enju_event\n", after: "enju_library\n"
-    inject_into_file 'app/assets/stylesheets/application.css',
-      " *= require enju_event/application\n", after: " *= require enju_leaf\n"
-    inject_into_file 'app/assets/javascripts/application.js',
-      "//= require enju_event/application\n", after: "//= require enju_leaf\n"
+    inject_into_file 'app/assets/stylesheets/enju_leaf/application.css',
+      " *= require enju_event/application\n", after: " *= require enju_leaf/application\n"
+    inject_into_file 'app/assets/javascripts/enju_leaf/application.js',
+      "//= require enju_event/application\n", after: "//= require enju_leaf/application\n"
   end
 end
