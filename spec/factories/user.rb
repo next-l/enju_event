3c6da94669b2b8a6c57c1beef08cc5ef061a7716
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-15 16:30:31 +0900

    add package.json

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,43 +1,37 @@
 FactoryBot.define do
-  factory :admin, class: User do |f|
-    f.sequence(:username){|n| "admin_#{n}"}
-    f.sequence(:email){|n| "admin_#{n}@example.jp"}
-    f.password { 'adminpassword' }
-    f.password_confirmation { 'adminpassword' }
-    f.after(:create) do |user|
-      user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'Administrator').id})
-      user_has_role.save
-      user.reload
+  factory :admin, class: User do
+    sequence(:username){|n| "admin_#{n}"}
+    sequence(:email){|n| "admin_#{n}@example.jp"}
+    password { 'adminpassword' }
+    password_confirmation { 'adminpassword' }
+    association :profile
+    after(:create) do |user|
+      user.role = Role.find_by(name: 'Administrator')
     end
   end
 
-  factory :librarian, class: User do |f|
-    f.sequence(:username){|n| "librarian_#{n}"}
-    f.sequence(:email){|n| "librarian_#{n}@example.jp"}
-    f.password { 'librarianpassword' }
-    f.password_confirmation { 'librarianpassword' }
-    f.after(:create) do |user|
-      user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'Librarian').id})
-      user_has_role.save
-      user.reload
+  factory :librarian, class: User do
+    sequence(:username){|n| "librarian_#{n}"}
+    sequence(:email){|n| "librarian_#{n}@example.jp"}
+    password { 'librarianpassword' }
+    password_confirmation { 'librarianpassword' }
+    association :profile
+    after(:create) do |user|
+      user.role = Role.find_by(name: 'Librarian')
     end
   end
 
-  factory :user, class: User do |f|
-    f.sequence(:username){|n| "user_#{n}"}
-    f.sequence(:email){|n| "user_#{n}@example.jp"}
-    f.password { 'userpassword' }
-    f.password_confirmation { 'userpassword' }
-    f.after(:create) do |user|
-      user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'User').id})
-      user_has_role.save
-      user.reload
+  factory :user, class: User do
+    sequence(:username){|n| "user_#{n}"}
+    sequence(:email){|n| "user_#{n}@example.jp"}
+    password { 'userpassword' }
+    password_confirmation { 'userpassword' }
+    association :profile
+    after(:create) do |user|
+      user.role = Role.find_by(name: 'User')
     end
   end
 
-  factory :invalid_user, class: User do |f|
+  factory :invalid_user, class: User do
   end
 end
