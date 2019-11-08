d820f1a18206a0ed5b3ff8ff405a48518f21c0d1
Kosuke Tanabe <nabeta@fastmail.fm>
2019-11-08 20:15:13 +0900

    add transition_class and initial_state

--- app/models/event_import_file_transition.rb
+++ app/models/event_import_file_transition.rb
@@ -5,3 +5,16 @@ class EventImportFileTransition < ApplicationRecord
   belongs_to :event_import_file, inverse_of: :event_import_file_transitions
 end
 
+# == Schema Information
+#
+# Table name: event_import_file_transitions
+#
+#  id                   :bigint           not null, primary key
+#  to_state             :string
+#  metadata             :jsonb
+#  sort_key             :integer
+#  event_import_file_id :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  most_recent          :boolean          not null
+#
