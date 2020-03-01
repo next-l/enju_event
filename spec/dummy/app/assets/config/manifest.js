0328d45c1dc8cdcb3e7d1022104a6a86ee8c7183
Kosuke Tanabe <nabeta@fastmail.fm>
2020-03-01 19:45:34 +0900

    add dummy asset files

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link icons/feed.png
+//= link icons/page_white_excel.png
+//= link icons/calendar.png
